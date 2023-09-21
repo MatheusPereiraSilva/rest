0181866086a18c3e9dd0fb7670c7255dbe6ac7f4
Matheus Pereira <133778757+MatheusPereiraSilva@users.noreply.github.com>
2023-09-21 17:47:24 -0300

    Upload do Projeto
    Upload dos arquivos do projeto

---
+++ rest/page-sobre.php
@@ -0,0 +1,25 @@
+<?php
+//Template Name: Sobre
+?>
+
+<?php get_header(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<section class="container sobre">
+			<h2 class="subtitulo"><?php the_title(); ?></h2>
+
+			<div class="grid-8">
+				<img src="<?php the_field('foto_rest'); ?>" alt="<?php the_field('foto_rest_descricao'); ?>">
+			</div>
+
+			<div class="grid-8">
+				<h2><?php the_field('titulo_historia'); ?></h2>
+				<p><?php the_field('texto_da_historia'); ?></p>
+				
+				<h2><?php the_field('titulo_visao'); ?></h2>
+				<p><?php the_field('texto_visao'); ?></p>
+				<h2><?php the_field('titulo_valores'); ?></h2>
+				<p><?php the_field('texto_valores'); ?></p>
+			</div>
+		</section>
+		<?php endwhile; else: endif; ?>
+<?php get_footer(); ?>	
